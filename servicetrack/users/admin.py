f35af14c2bb27466703ed1d46f3ab71e5898e5b2
Dmitry <pokemon2708@yandex.ru>
2025-12-04 18:56:45 +0300

    feat: add WorkerGroup model for team management
    - Create WorkerGroup model with name and description fields
    - Implement ManyToMany relationship with workers (CustomUser)
    - Add ForeignKey relationship with manager (CustomUser)
    - Integrate WorkerGroup into admin panel

--- servicetrack/users/admin.py
+++ servicetrack/users/admin.py
@@ -6,6 +6,26 @@ import django.contrib.auth.admin
 import users.models
 
 
+@django.contrib.admin.register(users.models.WorkerGroup)
+class WorkerGroupAdmin(django.contrib.admin.ModelAdmin):
+    model = users.models.WorkerGroup
+    fields = (
+        model.name.field.name,
+        model.description.field.name,
+        model.workers.field.name,
+        model.manager.field.name,
+        model.created_at.field.name,
+        model.updated_at.field.name,
+    )
+
+    readonly_fields = (
+        model.created_at.field.name,
+        model.updated_at.field.name,
+    )
+
+    filter_horizontal = (model.workers.field.name,)
+
+
 class ProfileInline(django.contrib.admin.StackedInline):
     model = users.models.Profile
     can_delete = False
@@ -17,6 +37,21 @@ class ProfileInline(django.contrib.admin.StackedInline):
 
 
 class UserAdmin(django.contrib.auth.admin.UserAdmin):
+    model = users.models.CustomUser
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    model.username.field.name,
+                    model.email.field.name,
+                    "password1",
+                    "password2",
+                ),
+            },
+        ),
+    )
     inlines = (ProfileInline,)
 
 
