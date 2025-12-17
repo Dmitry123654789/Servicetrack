1af11795c1ff4bc1060fcd6a6307e8276fabd15f
Arizel79 <198223737+Arizel79@users.noreply.github.com>
2025-12-17 18:14:56 +0300

    fix: error in editing company

--- servicetrack/company/forms.py
+++ servicetrack/company/forms.py
@@ -13,26 +13,27 @@ class WorkerGroupForm(django.forms.ModelForm):
         self.request = kwargs.pop("request")
         super().__init__(*args, **kwargs)
 
-        organization = self.request.user.profile.organization
-        user_model = django.contrib.auth.get_user_model()
+        if self.request:
+            organization = self.request.user.profile.organization
+            user_model = django.contrib.auth.get_user_model()
 
-        self.fields["manager"].queryset = user_model.objects.filter(
-            profile__organization=organization,
-            profile__role=users.models.Profile.Role.GROUP_MANAGER,
-        )
-
-        self.fields["workers"].queryset = user_model.objects.filter(
-            profile__organization=organization,
-        ).filter(
-            django.db.models.Q(
-                profile__role=users.models.Profile.Role.WORKER,
-            )
-            | django.db.models.Q(
+            self.fields["manager"].queryset = user_model.objects.filter(
+                profile__organization=organization,
                 profile__role=users.models.Profile.Role.GROUP_MANAGER,
-            ),
-        )
+            )
 
-        self.fields["workers"].widget = django.forms.CheckboxSelectMultiple()
+            self.fields["workers"].queryset = user_model.objects.filter(
+                profile__organization=organization,
+            ).filter(
+                django.db.models.Q(
+                    profile__role=users.models.Profile.Role.WORKER,
+                )
+                | django.db.models.Q(
+                    profile__role=users.models.Profile.Role.GROUP_MANAGER,
+                ),
+            )
+
+            self.fields["workers"].widget = django.forms.CheckboxSelectMultiple()
 
     def clean_workers(self):
         workers_queryset = self.cleaned_data.get("workers")
@@ -46,7 +47,7 @@ class WorkerGroupForm(django.forms.ModelForm):
         return workers_queryset
 
 
-class OrganizationForm(WorkerGroupForm):
+class OrganizationForm(django.forms.ModelForm):
     class Meta:
         model = company.models.Organization
         fields = (
